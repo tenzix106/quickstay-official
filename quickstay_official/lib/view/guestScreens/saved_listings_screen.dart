21535ac1ab0ad27432fd225616c6096a320f31bc
JoachimWong <129176847+JoachimWong@users.noreply.github.com>
2024-11-01 09:20:30 +0800

    Joachim - Done
    Select Booking Dates, Make Booking and Payment

--- quickstay_official/lib/view/guestScreens/saved_listings_screen.dart
+++ quickstay_official/lib/view/guestScreens/saved_listings_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quickstay_official/model/app_constants.dart';
+import 'package:quickstay_official/model/posting_model.dart';
+import 'package:quickstay_official/widgets/posting_grid_tile_ui.dart';
+import 'package:quickstay_official/widgets/view_posting_screen.dart';
 
 class SavedListingsScreen extends StatefulWidget {
   const SavedListingsScreen({super.key});
@@ -10,7 +15,65 @@ class SavedListingsScreen extends StatefulWidget {
 class _SavedListingsScreenState extends State<SavedListingsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(25, 15, 25, 0),
+      child: GridView.builder(
+          physics: const ScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: AppConstants.currentUser.savedPostings!.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 15,
+            mainAxisSpacing: 15,
+            childAspectRatio: 3 / 4,
+          ),
+          itemBuilder: (context, index) {
+            PostingModel currentPosting =
+                AppConstants.currentUser.savedPostings![index];
+
+            return Stack(
+              children: [
+                InkResponse(
+                  enableFeedback: true,
+                  child: PostingGridTileUi(
+                    posting: currentPosting,
+                  ),
+                  onTap: () {
+                    Get.to(ViewPostingScreen(
+                      posting: currentPosting,
+                    ));
+                  },
+                ),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 10.0),
+                    child: Container(
+                      width: 30,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                      ),
+                      child: IconButton(
+                        onPressed: () {
+                          AppConstants.currentUser
+                              .removeSavedPosting(currentPosting);
+
+                          setState(() {});
+                        },
+                        padding: const EdgeInsets.all(0),
+                        icon: const Icon(
+                          Icons.clear,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                //
+              ],
+            );
+          }),
     );
   }
 }
